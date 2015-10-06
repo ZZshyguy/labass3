680096abbcfb009c3555e35f376a03d622ce83fa
Zach Lapointe <zachlap64@gmail.com>
2015-10-06 12:43:00 -0600

    CS2010 Lab3 Finished

--- Labass3.cpp
+++ Labass3.cpp
@@ -1,56 +1,111 @@
 #include <cstdio>
 #include <cctype>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 /*
 Zachary Lapointe
+CS2010
 Assignment 3:
-Read a file and make a dictionary of words found
+Read a file and make a dictionary of unique words found
+Has space for 10 unique words (ROW)
 */
 
-const int ROW = 5;
+const int ROW = 10;
 const int COL = 20;
+int NumWords = 0; 	//number of words in dictionary
+int CNT[ROW]; 		//number of each individual words in dictionary
+char dic[ROW][COL]; 	//char array with 5 rows and 20 columns
+int count = 0;		//Counht of words not able to be put into dictionary
+
+int searchDic(char *buffer)	//Search dictionary for buffer, returns a 1 if found
+{
+
+	int found = 0;
+	int i;
+	for(i=0;i<NumWords;i++)
+		if(!strcmp(dic[i],buffer))	//strcmp() returns 0 if they are equal
+		{
+			CNT[i]++;
+			found = 1;
+			break;
+		};
+	return found;
+
+}
+
+void addwordtoDic(char *buffer)	//Add word to dictionary
+{
+
+	if(NumWords<ROW){
+		strcpy(dic[NumWords],buffer);
+		CNT[NumWords] = 1;
+		NumWords++;
+	}
+	else{
+		count++;
+	}
+	
+}
+
+void outputDic(void) 	//Displays words in dictionary and the count of how many times that word appeared, also prints count of words not included
+{
+
+	cout<<"Word              Frequency"<<endl;
+	cout<<"---------------------------"<<endl;
+
+	//for(int i=0;i< NumWords; i++) cout<<dic[i]<<"    "<<CNT[i]<<endl; Alternate printing method (not formatted)
+	for(int i=0; i<ROW; i++)
+	{
+	cout<<dic[i];
+	for(int j=0; j< 20- strlen(dic[i]); j++) cout<< ' ';
+	cout<<CNT[i]<<endl;
+	}
+
+	cout<<endl<<"Number of words not included =    "<<count<<endl;
+
+}
 
 int main() {
 
 	char ch;
-	int newLine= 0;
-	char dic[ROW][COL]; //char array with 5 rows and 20 columns
-	// cout<<a[0]; will print string in first row (array) of a
-	// for(i=0;i< NewWords; i++) cout<<a[i]<<endl; will print strings in the rows (arrays) of a
-	int len = 0;
-	char buffer[256]; // collect the word in a buffer, LEN is its length STRLEN(buffer); will return a length
-	int NumWords; //number of words in dictionary
-	int CNT[ROW]; //number of each individual words in dictionary
-	/*
-	int i;
-	for(i=0;i<NumWords;i++)
-		if(!strcmp(a[i],buffer)){CNT[i]++;break;}; //Compares word in buffer to words in dictionary
-	if(i==NumWords){addwordto a[NumWords]; NumWords++;} //if word not found put it into array
-	*/
+	int newLine = 0;
+	int len = 0;		//Length of word collected in buffer
+	char buffer[256]; 	//collect the word in a buffer, len is its length or STRLEN(buffer); will return length
+
+	for(int i=0; i<ROW; i++)
+	{
+		
+	}
 	
 	while( cin.good() )
 	{
 	 ch = cin.get();
-	 if ( isalpha(ch) )  // function test is ch  is A-Z or a-z, then prints chars
+	 if ( isalpha(ch) )  	// function test is ch  is A-Z or a-z, then prints chars
 	    {
 	       buffer[len++] = ch;
 	       newLine = 1;
 	    }
 	 else
-	       if (newLine) //were you making a word? if word is over end line
+	       if (newLine) 	//were you making a word? if word is over end line
 		  {
-		      if(!searchDic(buffer, dic)){ //Write searchDic
-			addwordto(dic[NumWords], buffer); //Write addwordto
-			NumWords++;
+		      buffer[len] = '\0';	//Makes buffer a string
+		      if(strlen(buffer)>19){	//Checks word length
+		        cout<<"Word too long"<<endl;
+			count++;
+		      }
+		      else{
+			      if(!searchDic(buffer)){ 			
+				addwordtoDic(buffer); 	
+			      }
 		      }
 		      len = 0;	
 		      newLine = 0;
 		  }
 	}
-	outputDic(dic, CNT); //Write outputDic
+	outputDic(); 
 	return 0;
 }
 
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
-Craps : Labass3.o
-	g++ Labass3.o -o Craps
+Dictionary : Labass3.o
+	g++ Labass3.o -o Dictionary
 
 
-Labass2.o : Labass3.cpp
+Labass3.o : Labass3.cpp
 	g++ -c Labass3.cpp
 
 clean:
